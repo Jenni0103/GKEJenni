77caca470195301f260c22e0bd259c8b0ad2c3f9
Jenni0103 <149856439+Jenni0103@users.noreply.github.com>
2023-11-06 19:54:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('Nama : Jennifer Vanny Winata \n')
-    res.send('NIM : 51022008\n')
-    res.send('Jurusan : Sistem Informasi \n')
 })
 
 app.listen(PORT, HOST)
